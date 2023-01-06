949e02e7a944807ee1d4d2f73e79d2207bb7cdb7
ioannablou <115368370+ioannablou@users.noreply.github.com>
2023-01-06 14:12:42 +0200

    Add files via upload

--- AthletesActions.java
+++ AthletesActions.java
@@ -1,9 +1,9 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-    import java.util.Scanner;
-	
 
 public class AthletesActions {
-    
+
 	private Athletes athlete;
 	public AthletesActions(Athletes a) {
 		athlete = a;
@@ -18,16 +18,32 @@ public class AthletesActions {
 
 	public void seeWholeConversation(Users receiver) {
 			Chat.seeConversation(athlete.getMessages(), receiver);
-			System.out.println("Do you want to send a new message? Type 1 for YES 2 for NO");
+			System.out.println("Do you want to send a new message? Type 1 for YES 2 for NO :");
 			Scanner in = new Scanner(System.in);
-			int reachout = in.nextInt();
+			boolean continueLoop = true;
+			int reachout = 0;
+			do {
+				try {
+                     reachout = in.nextInt();
+					while( reachout !=1 && reachout !=2 ) {
+						System.out.println("Wrong answer.Please type 1 for YES 2 for NO :");
+				  	    reachout = in.nextInt();
+					}
+			       continueLoop = false;
+				} catch(InputMismatchException e) {
+					System.out.println("You must enter a number. Please type 1 for YES 2 for NO :");
+					in.nextLine();
+				}
+			} while (continueLoop);
+
+
 			if (reachout == 1) {
 				Chat.sendMessage(receiver,athlete, Chat.checkChat(receiver, athlete));
 			}
 		}
 
-		public void sendNewMessage(Users receiver) {
-			Chat.sendMessage(receiver,athlete, Chat.checkChat(receiver, athlete));
+	public void sendNewMessage(Users receiver) {
+		Chat.sendMessage(receiver,athlete, Chat.checkChat(receiver, athlete));
 	}
 
 
